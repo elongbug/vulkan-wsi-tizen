f64ee8be1a4d037cdf136c4053a9a2bd57ca6d8f
deasung.kim <deasung.kim@samsung.com>
2016-12-14 18:08:20 +0900

    vk_tizen: added new function prototype for tpl_tbm support
    VKAPI_ATTR VkResult VKAPI_CALL vkCreateTBMQueueSurfaceKHR(VkInstance instance,
    														  const tbm_bufmgr bufmgr,
    														  const tbm_surface_queue_h queue,
    														  const VkAllocationCallbacks *pAllocator,
    														  VkSurfaceKHR *surface);
    
    Change-Id: I0d3acaf8fbaa985dee34be64a2906da239eb9b07

--- include/vulkan/vk_tizen.h
+++ include/vulkan/vk_tizen.h
@@ -3,6 +3,8 @@
 
 #include <vulkan/vulkan.h>
 #include <tbm_surface.h>
+#include <tbm_surface_queue.h>
+#include <tbm_bufmgr.h>
 
 typedef VkResult (VKAPI_PTR *PFN_vkCreateImageFromNativeBufferTIZEN)(VkDevice device, tbm_surface_h
 																	 surface, const
@@ -15,6 +17,12 @@ typedef VkResult (VKAPI_PTR *PFN_vkQueueSignalReleaseImageTIZEN)
 typedef VkResult (VKAPI_PTR *PFN_vkAcquireImageTIZEN)
 	(VkDevice device, VkImage image, int nativeFenceFd,
 	 VkSemaphore semaphore, VkFence fence);
+typedef VkResult (VKAPI_PTR *PFN_vkCreateTBMQueueSurfaceKHR)
+	(VkInstance instance,
+	 const tbm_bufmgr bufmgr,
+	 const tbm_surface_queue_h queue,
+	 const VkAllocationCallbacks *pAllocator,
+	 VkSurfaceKHR *surface);
 
 #ifndef VK_NO_PROTOTYPES
 VKAPI_ATTR VkResult VKAPI_CALL vkCreateImageFromNativeBufferTIZEN(
@@ -23,12 +31,20 @@ VKAPI_ATTR VkResult VKAPI_CALL vkCreateImageFromNativeBufferTIZEN(
 	const VkImageCreateInfo *					pCreateInfo,
 	const VkAllocationCallbacks *				pAllocator,
 	VkImage *									pImage);
-VKAPI_PTR VkResult vkQueueSignalReleaseImageTIZEN(VkQueue queue, uint32_t waitSemaphoreCount,
-												  const VkSemaphore *pWaitSemaphores,
-												  VkImage image, int *pNativeFenceFd);
-VKAPI_PTR VkResult vkAcquireImageTIZEN(VkDevice device, VkImage image, int nativeFenceFd,
-									   VkSemaphore semaphore, VkFence fence);
-
+VKAPI_ATTR VkResult VKAPI_CALL vkQueueSignalReleaseImageTIZEN(VkQueue queue,
+															  uint32_t waitSemaphoreCount,
+															  const VkSemaphore *pWaitSemaphores,
+															  VkImage image, int *pNativeFenceFd);
+VKAPI_ATTR VkResult VKAPI_CALL vkAcquireImageTIZEN(VkDevice device, VkImage image,
+												   int nativeFenceFd,
+												   VkSemaphore semaphore, VkFence fence);
+VKAPI_ATTR VkResult VKAPI_CALL vkCreateTBMQueueSurfaceKHR(VkInstance instance,
+														  const tbm_bufmgr bufmgr,
+														  const tbm_surface_queue_h queue,
+														  const VkAllocationCallbacks *pAllocator,
+														  VkSurfaceKHR *surface);
 #endif
+
+#define VK_ICD_WSI_PLATFORM_TBM_QUEUE 0x0000000F
 
 #endif /* VK_TIZEN_H */
